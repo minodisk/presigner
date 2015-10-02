d5a9bab7b28317f29369450ee988568a3cb753df
minodisk <daisuke.mino@gmail.com>
2015-10-02 20:34:58 +0900

    Output errors

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/go-microservices/presigner/option"
@@ -9,22 +9,32 @@ import (
 )
 
 func main() {
+	err := run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() (err error) {
 	var (
-		o   option.Options
-		err error
+		o option.Options
 	)
 
 	o, err = option.New(os.Args[1:])
 	if err != nil {
-		os.Exit(1)
+		return
 	}
+
 	err = o.ReadPrivateKey()
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 
 	err = server.Serve(o)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
+
+	return
 }
